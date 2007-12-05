7e63bbd7d49e928be63eafdd2e690d06c3a0a3f7
avp <avp@65d2813f-f11f-0410-b17b-8647a28db151>
2007-12-05 00:00:56 +0000

    Works, but needs improvement.
    git-svn-id: file:///Users/avp/tmp/svn.20121212/LHPC/aff/trunk@383 65d2813f-f11f-0410-b17b-8647a28db151

--- math/aff.m
+++ math/aff.m
@@ -1,118 +1,144 @@
-aff = OpenRead["/Users/avp/Desktop/xx/data/bb_cfg1098.aff",
-               BinaryFormat -> True];
-(*
-aff = OpenRead["/Users/avp/Desktop/xx/data/out.aff",
-               BinaryFormat -> True];
+(* Reading interface for AFF files
+ *  aff = affOpen[fileName] ; -- open an aff file and prepares it for access
+ *  {type, data} = affGet(aff, keyPathString); -- extract data for a given key
+ *  affClose[aff];   -- close the file and free the resources
  *)
+affOpen[fileName_String] :=
+  Module[{i, aff, affOpened, readHeader,
+	  idString, dSize, dBase, dMant, dMaxExp, dMinExp, hSize,
+	  dtOff, dtSize, dtCount,
+	  stOff, stSize, stCount,
+	  tbOff, tbSize, tbCount,
+	  affSym, nodeTypes, affRoot, affNodes, affName, affOffset, affSize,
+	  affType, affChildren,
+	  affXGet, affXClose,
+          name, type, offset, size, children},
+    affOpened = False;
+    aff = OpenRead[fileName, BinaryFormat -> True];
 
-readHeader[f_InputStream] :=
-  Module[{o,s,c,m},
-	 o = BinaryRead[f,"UnsignedInteger64", ByteOrdering->1];
-	 s = BinaryRead[f,"UnsignedInteger64", ByteOrdering->1];
-	 c = BinaryRead[f,"UnsignedInteger64", ByteOrdering->1];
-	 m = BinaryRead[f,"UnsignedInteger128", ByteOrdering->1];
-	 {o,s,c}];
+    If [aff == $Failed, Return[$Failed]];
 
-idString = BinaryRead[aff, "TerminatedString"];
-dSize = BinaryRead[aff, "Byte"];
-dBase = BinaryRead[aff, "Byte"];
-dMant = BinaryRead[aff, "Byte"];
-dMaxexp = BinaryRead[aff, "UnsignedInteger16", ByteOrdering->1];
-dMinexp = BinaryRead[aff, "UnsignedInteger16", ByteOrdering->1];
-hSize = BinaryRead[aff, "UnsignedInteger32", ByteOrdering->1];
-{dtOff, dtSize, dtCount} = readHeader[aff];
-{stOff, stSize, stCount} = readHeader[aff];
-{tbOff, tbSize, tbCount} = readHeader[aff];
+    readHeader[f_InputStream] :=
+      Module[{o,s,c,m},
+	     o = BinaryRead[f,"UnsignedInteger64", ByteOrdering->1];
+	     s = BinaryRead[f,"UnsignedInteger64", ByteOrdering->1];
+	     c = BinaryRead[f,"UnsignedInteger64", ByteOrdering->1];
+	     m = BinaryRead[f,"UnsignedInteger128", ByteOrdering->1];
+	     {o,s,c}];
 
-Print[StringJoin["data: offset ", ToString[dtOff],
-		 ", size ", ToString[dtSize],
-		 ", count ", ToString[dtCount]]];
-Print[StringJoin["stable: offset ", ToString[stOff],
-		 ", size ", ToString[stSize],
-		 ", count ", ToString[stCount]]];
-Print[StringJoin["tree: offset ", ToString[tbOff],
-		 ", size ", ToString[tbSize],
-		 ", count ", ToString[tbCount]]];
+    idString = BinaryRead[aff, "TerminatedString"];
+    dSize = BinaryRead[aff, "Byte"];
+    dBase = BinaryRead[aff, "Byte"];
+    dMant = BinaryRead[aff, "Byte"];
+    dMaxexp = BinaryRead[aff, "UnsignedInteger16", ByteOrdering->1];
+    dMinexp = BinaryRead[aff, "UnsignedInteger16", ByteOrdering->1];
+    hSize = BinaryRead[aff, "UnsignedInteger32", ByteOrdering->1];
+    {dtOff, dtSize, dtCount} = readHeader[aff];
+    {stOff, stSize, stCount} = readHeader[aff];
+    {tbOff, tbSize, tbCount} = readHeader[aff];
 
-(* read symbol table *)
-affSym = Table[False, {stCount}];
-SetStreamPosition[aff, stOff];
-For[i=1, i <= stCount, i++, affSym[[i]]=BinaryRead[aff,"TerminatedString"]];
+    (* read symbol table *)
+    affSym = Table[False, {stCount}];
+    SetStreamPosition[aff, stOff];
+    For[i=1, i <= stCount, i++, affSym[[i]]=BinaryRead[aff,"TerminatedString"]];
 
-(* read nodes *)
-nodeTypes = {"void", "char", "int", "double", "complex"};
-affRoot = {};
-affNodes    = Table[False, {tbCount}];
-affName     = Table[False, {tbCount}];
-affOffset   = Table[False, {tbCount}];
-affSize     = Table[False, {tbCount}];
-affType     = Table[False, {tbCount}];
-affChildren = Table[{}, {tbCount}];
+    (* read nodes *)
+    nodeTypes = {"void", "char", "int", "double", "complex"};
+    affRoot = {};
+    affNodes    = Table[False, {tbCount}];
+    affName     = Table[False, {tbCount}];
+    affOffset   = Table[False, {tbCount}];
+    affSize     = Table[False, {tbCount}];
+    affType     = Table[False, {tbCount}];
+    affChildren = Table[{}, {tbCount}];
 
-Print["Stable loaded"];
+    SetStreamPosition[aff,tbOff];
+    For[i = 1, i <= tbCount, i++,
+	Module[{t,p,n,s,o},
+	       t = nodeTypes[[BinaryRead[aff,"Byte"]]];
+	       p = BinaryRead[aff, "UnsignedInteger64", ByteOrdering -> 1];
+	       n = affSym[[BinaryRead[aff, "UnsignedInteger32",
+				      ByteOrdering -> 1]+1]];
+	       If [t == "void", s = 0; o = 0,
+		     s = BinaryRead[aff, "UnsignedInteger32", ByteOrdering->1];
+		     o = BinaryRead[aff, "UnsignedInteger64", ByteOrdering->1]];
+	       affName[[i]] = n;
+	       affOffset[[i]] = o;
+	       affSize[[i]] = s;
+	       affType[[i]] = t;
+	       If [p == 0, affRoot = Append[affRoot,i],
+		     affChildren[[p]] = Append[affChildren[[p]], i]]]];
 
-readNode[f_InputStream, i_] :=
-  Module[{t,p,n,s,o},
-	 t = nodeTypes[[BinaryRead[f,"Byte"]]];
-         p = BinaryRead[f, "UnsignedInteger64", ByteOrdering -> 1];
-         n = affSym[[BinaryRead[f, "UnsignedInteger32", ByteOrdering -> 1]+1]];
-         If [t == "void", s = 0; o = 0,
-	       s = BinaryRead[f, "UnsignedInteger32", ByteOrdering->1];
-	       o = BinaryRead[f, "UnsignedInteger64", ByteOrdering->1]];
-	 affName[[i]] = n;
-	 affOffset[[i]] = o;
-	 affSize[[i]] = s;
-	 affType[[i]] = t;
-	 If [p == 0, affRoot = Append[affRoot,i],
-               affChildren[[p]] = Append[affChildren[[p]], i]];
-	 True]
+    For[i = tbCount, i>= 1, i--,
+	affNodes[[i]] = {name   -> affName[[i]],
+			 type   -> affType[[i]],
+			 offset -> affOffset[[i]],
+			 size   -> affSize[[i]],
+			 children -> Map[Function[c,
+						  affName[[c]]->affNodes[[c]]],
+					 affChildren[[i]]]}];
 
-SetStreamPosition[aff,tbOff];
-For[i = 1, i <= tbCount, i++, readNode[aff,i]];
+    root = Map[Function[c,affName[[c]]->affNodes[[c]]], affRoot];
 
-Print["Tree loaded"];
+    affXGet[aO_, path_String] :=
+    Module[{t,o,s,d,fmt,lookup},
+	   If [affOpened == False, Return[$Failed]];
+	   lookup = Module[{ks, d, ch},
+			   ks = StringSplit[path, "/"];
+			   If [ks == {}, Return[$Failed]];
+			   d = First[ks] /. root;
+			   If [d == First[ks], Return[$Failed]];
+			   For [ks = Rest[ks],
+				   ks != {},
+				   ch = children /. d;
+				   If [ch == children, Return[$Failed]];
+				   d = First[ks] /. ch;
+				   ks = Rest[ks]];
+			   If [ListQ[d], {type/.d, offset/.d, size/.d},
+				    Return[$Failed]]];
+	   If [lookup == $Failed, Return [$Failed]];
+	   {t, o, s} = lookup;
+	   fmt = Switch[t,
+			"void", Return[{t,0,{}}],
+			"char", "Character8",
+			"int",  "Integer32",
+			"double", "Real64",
+			"complex", "Complex128",
+			_, Return[$Failed]];
+	   SetStreamPosition[aff, o];
+	   d = BinaryReadList[aff, fmt, s, ByteOrdering -> 1];
+	   {t, d}];
 
-buildNode[i_] :=
-  affNodes[[i]] = {name   -> affName[[i]],
-		   type   -> affType[[i]],
-		   offset -> affOffset[[i]],
-		   size   -> affSize[[i]],
-                   children -> Map[Function[c, affName[[c]]->affNodes[[c]]],
-				   affChildren[[i]]]};
+    affXClose[aO_] := 
+      Module[{},
+	     If [affOpened == False, Return[]];
+	     affOpened = False;
+	     Close[aff]; aff =.;
+	     i =. ;
+	     root = .;
+	     idString =. ;
+	     dSize =. ;
+	     dBase =. ;
+	     dMant =. ;
+	     dMaxexp =. ;
+	     dMinexp =. ;
+	     hSize =. ;
+	     dtOff =. ; dtSize =. ; dtCount =. ;
+	     stOff =. ; stSize =. ; stCount =. ;
+	     tbOff =. ; tbSize =. ; tbCount =. ;
+	     affSym =. ;
+	     nodeTypes =. ;
+	     affRoot =. ;
+	     affNodes    =. ;
+	     affName     =. ;
+	     affOffset   =. ;
+	     affSize     =. ;
+	     affType     =. ;
+	     affChildren =. ;
+	     affXGet =.;
+	     affXClose =. ;];
+    affOpened = True;
+    { affXGet, affXClose }];
 
-For[i = tbCount, i>= 1, i--, buildNode[i]];
-
-root = Map[Function[c,affName[[c]]->affNodes[[c]]], affRoot];
-
-Print["Tree built"];
-
-resolveKey[path_String] :=
-  Module[{ks, d, ch},
-	 ks = StringSplit[path, "/"];
-         If [ks == {}, {"void", 0, 0},
-                d = First[ks] /. root;
-                If [d == First[ks], Return[$Failed]];
-		For [ks = Rest[ks],
-			ks != {},
-			ch = children /. d;
-			If [ch == children, Return[$Failed]];
-			d = First[ks] /. (children /. d);
-			ks = Rest[ks]];
-		If [ListQ[d], Return[{type /. d, offset /. d, size /. d}]];
-		$Failed]];
-
-getData[path_String] :=
-  Module[{t,o,s,d,fmt},
-	 {t,o,s} = resolveKey[path];
-	 fmt = Switch[t,
-		      "void", Return[{t,0,{}}],
-		      "char", "Character8",
-		      "int", "Integer32",
-		      "double", "Real64",
-		      "complex", "Complex128",
-		      _, Return[$Failed]];
-	 SetStreamPosition[aff,o];
-	 d = BinaryReadList[aff,fmt,s, ByteOrdering->1];
-	 {t,s,d}];
-
-	 
+affGet[aff_, path_String] := aff[[1]][aff, path];
+affClose[aff_] := aff[[2]][aff];
