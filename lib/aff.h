c788afa44fc9db06cb8f987ce9e37caff89441b1
avp <avp@65d2813f-f11f-0410-b17b-8647a28db151>
2009-09-07 23:12:15 +0000

    AFF version 2.1.0 marked. NB: node_cda() appears unimplemented.
    git-svn-id: file:///Users/avp/tmp/svn.20121212/LHPC/aff/releases/lhpc-aff-2.1.0@1028 65d2813f-f11f-0410-b17b-8647a28db151

--- lib/aff.h
+++ lib/aff.h
@@ -2,7 +2,7 @@
 #define MARK_c7329614_7d7a_4efe_9f6d_87477961bc99
 
 /* AFF library information */
-#define AFF_VERSION "XXX Version 2.1.0x $Id$"
+#define AFF_VERSION "Version 2.1.0 $Id$"
 const char *aff_version(void);
 int aff_name_check(const char *name);
 
